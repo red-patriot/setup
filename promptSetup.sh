834c0b1b7fb14b6379e09945cb3c901850d40e4d
Bryson Tanner <bltanner105@gmail.com>
2024-02-15 15:39:39 -0600

    feat: Add bash prompt customization options

---
+++ promptSetup.sh
@@ -0,0 +1,6 @@
+# These set the desired behavior of bash-git-prompt
+# Paste them into .bashrc
+GIT_PROMPT_ONLY_IN_REPO=0
+GIT_PROMPT_FETCH_REMOTE_STATUS=0
+GIT_PROMPT_IGNORE_SUBMODULES=1
+GIT_PROMPT_THEME=RP
