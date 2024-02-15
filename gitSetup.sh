c4dec43f0ad5e54ade35931dfd60b2bf40cd1a3b
Bryson Tanner <bltanner105@gmail.com>
2024-02-15 10:23:18 -0600

    feat: Add git setup script
    Adds aliases and some simple behaviors

---
+++ gitSetup.sh
@@ -0,0 +1,15 @@
+# Setup useful aliases that I like
+git config --global alias.fap 'fetch --all --prune'
+git config --global alias.pushfl 'push --force-with-lease'
+git config --global alias.stats 'status'
+git config --global alias.graph 'log --graph --all --format="%h %s%n    (%an, %ar)" --abbrev-commit'
+git config --global alias.gone '! git branch -vv | grep ": gone]"|  grep -v "\*" | awk "{ print $1; }" | xargs -r git branch -D'
+git config --global alias.staash 'stash --all'
+
+# Enable Reuse Recorded Resolution
+git config --global rerere.unabled true
+git config --global rerere.autoUpdate true
+
+# Make displaying branches nicer
+git config --global column.ui auto
+git config --global branch.sort -committerdate
