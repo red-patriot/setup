75505ab2f1ded49f663e26ad011f75e3a7869a89
Bryson Tanner <sandwichman1618@gmail.com>
2024-02-19 23:06:24 -0600

    feat: Add script to help automate setup on Arch Linux

---
+++ archSetup.sh
@@ -0,0 +1,20 @@
+# Prettify pacman output
+sudo echo 'Color' > /etc/pacman.conf
+sudo echo 'ILoveCandy' > /etc/pacman.conf
+
+sudo pacman -Syu
+
+# Install needed packages
+pacman -S --needed git base-devel cmake gcc pacman-contrib zoxide
+
+# Setup paccache timer to clean temp files
+sudo systemctl enable paccache.timer
+
+# Setup yay
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Y --gendb
+
+sudo yay bash-git-prompt-git
