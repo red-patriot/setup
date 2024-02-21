88cd8202ae2c54cf5073feb28cddf2370802443d
Bryson Tanner <sandwichman1618@gmail.com>
2024-02-21 16:57:00 -0600

    feat: Add emacs as the default code editor

--- gitSetup.sh
+++ gitSetup.sh
@@ -5,6 +5,7 @@ git config --global alias.stats 'status'
 git config --global alias.graph 'log --graph --all --format="%h %s%n    (%an, %ar)" --abbrev-commit'
 git config --global alias.gone '! git branch -vv | grep ": gone]"|  grep -v "\*" | awk "{ print $1; }" | xargs -r git branch -D'
 git config --global alias.staash 'stash --all'
+git config --global core.editor emacs
 
 # Enable Reuse Recorded Resolution
 git config --global rerere.unabled true
