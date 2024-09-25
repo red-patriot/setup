3bd5604a74ce5f281829e1f648f426c134f6f787
Bryson Tanner <bltanner105@gmail.com>
2024-09-24 21:59:50 -0500

    feat: Install guake on startup

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -1,8 +1,9 @@
 # Setup git
 source ./gitSetup.sh
 
-# Install Emacs
+# Install Packages
 sudo apt install emacs -y
+sudo apt install guake -y
 
 # Set up git prompt
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
