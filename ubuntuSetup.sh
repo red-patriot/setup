614576d037aa27de64b50f52b98a3352fc20b448
Bryson Tanner <bltanner105@gmail.com>
2024-09-25 18:59:31 -0500

    chore: Install development tools

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -4,6 +4,9 @@ source ./gitSetup.sh
 # Install Packages
 sudo apt install emacs -y
 sudo apt install guake -y
+sudo apt install g++ -y
+sudo apt install gcc -y
+sudo apt install cmake -y
 
 # Set up git prompt
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
