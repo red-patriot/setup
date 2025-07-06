d832efb87ab678882eb11afd2cd9e2c901ac13aa
Bryson Tanner <bltanner105@gmail.com>
2025-07-06 11:53:18 -0500

    chore: Add vscode setup

---
+++ vscodeSetup.sh
@@ -0,0 +1,26 @@
+code --install-extension \
+    azemoh.one-monokai \
+    bradlc.vscode-tailwindcss \
+    charliermarsh.ruff \
+    cheshirekow.cmake-format \
+    emmanuelbeziat.vscode-great-icons \
+    esbenp.prettier-vscode \
+    github.copilot \
+    github.copilot-chat \
+    matepek.vscode-catch2-test-adapter \
+    ms-python.debugpy \
+    ms-python.mypy-type-checker \
+    ms-python.python \
+    ms-python.vscode-pylance \
+    ms-vscode.cmake-tools \
+    ms-vscode.cpptools \
+    ms-vscode.cpptools-extension-pack \
+    ms-vscode.cpptools-themes \
+    pkief.material-icon-theme \
+    redhat.vscode-xml \
+    tamasfe.even-better-toml \
+    twxs.cmake \
+    vitest.explorer \
+    zhuangtongfa.material-theme \
+
+cat settings.json > ~/.config/Code/User/settings.json
